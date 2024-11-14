8f34959646f85ba54db28164ee838298f7a87af9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-11-13 23:28:39 -0800

    Get validation of the set details.

---
+++ include/gesel/check_set_details.hpp
@@ -0,0 +1,72 @@
+#ifndef GESEL_CHECK_INDICES_HPP
+#define GESEL_CHECK_INDICES_HPP
+
+#include <string>
+#include <cstdint>
+#include <vector>
+
+#include "byteme/byteme.hpp"
+
+#include "parse_field.hpp"
+
+namespace gesel {
+
+namespace internal {
+
+void check_set_details(const std::string& path, const std::vector<int32_t>& ranges, const std::vector<int32_t>& sizes) {
+    byteme::RawFileReader raw_r(path);
+    auto gzpath = path + ".gz";
+    byteme::GzipFileReader gzip_r(gzpath);
+
+    byteme::PerByte raw_p(&raw_r);
+    byteme::PerByte gzip_p(&gzip_r);
+
+    bool raw_valid = raw_p.valid();
+    bool gzip_valid = gzip_p.valid();
+    size_t line = 1;
+    size_t num_ranges = ranges.size();
+
+    while (raw_valid) {
+        auto raw_pos = raw_p.position();
+        auto name = parse_string_field<FieldType::MIDDLE>(raw_p, raw_valid, path, line);
+        auto description = parse_string_field<FieldType::LAST>(raw_p, raw_valid, path, line);
+
+        if (line > num_ranges) {
+            throw std::runtime_error("number of lines in '" + path + "' exceeds that expected from its '*.ranges.gz' file (line " + std::to_string(line) + ")");
+        }
+        if (raw_p.position() - raw_pos - 1 != static_cast<size_t>(ranges[line - 1])) {
+            throw std::runtime_error("number of bytes per line in '" + path + "' is not the same as that expected from the '*.ranges.gz' file (line " + std::to_string(line) + ")");
+        }
+
+        if (!gzip_valid) {
+            throw std::runtime_error("early termination of the Gzipped version of '" + path + "'");
+        }
+
+        auto gz_name = parse_string_field<FieldType::MIDDLE>(gzip_p, gzip_valid, path, line);
+        if (gz_name != name) {
+            throw std::runtime_error("different name in '" + path + "' compared to its Gzipped version (line " + std::to_string(line) + ")");
+        }
+
+        auto gz_description = parse_string_field<FieldType::MIDDLE>(gzip_p, gzip_valid, path, line);
+        if (gz_description != description) {
+            throw std::runtime_error("different description in '" + path + "' compared to its Gzipped version (line " + std::to_string(line) + ")");
+        }
+
+        auto gz_size = parse_integer_field<FieldType::LAST>(gzip_p, gzip_valid, path, line);
+        if (gz_size != sizes[line - 1]) {
+            throw std::runtime_error("different size in '" + path + ".gz' compared to its '*.ranges.gz' file (line " + std::to_string(line) + ")");
+        }
+
+        ++line;
+    }
+
+    if (line - 1 != num_ranges) {
+        throw std::runtime_error("number of lines in '" + path + "' is less than that expected from its '*.ranges.gz' file (line " + std::to_string(line) + ")");
+    }
+}
+
+}
+
+}
+
+#endif
---
+++ tests/src/check_set_details.cpp
@@ -0,0 +1,98 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "gesel/check_set_details.hpp"
+#include "byteme/temp_file_path.hpp"
+
+#include "utils.h"
+
+TEST(CheckSetDetails, Success) {
+    auto path = byteme::temp_file_path("check_sets");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+
+    std::vector<int32_t> ranges { 31, 27 };
+    std::vector<int32_t> sizes { 51, 82 };
+    gesel::internal::check_set_details(path, ranges, sizes);
+}
+
+TEST(CheckSetDetails, Failure) {
+    auto path = byteme::temp_file_path("check_sets");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's\tset\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 51, 82 }); }, "tab");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's\nset\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 51, 82 }); }, "newline");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31 }, std::vector<int32_t>{ 51 }); }, "number of lines");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 28 }, std::vector<int32_t>{ 51, 82 }); }, "bytes per line");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 51, 82 }); }, "early termination");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nfoo bar\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 51, 82 }); }, "different name");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tfoobar\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 51, 82 }); }, "different description");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27 }, std::vector<int32_t>{ 52, 82 }); }, "different size");
+
+    {
+        byteme::RawFileWriter rwriter(path);
+        rwriter.write("aaron's set\tthis is aaron's set\nanother set\tyet another set\n");
+        byteme::GzipFileWriter gwriter(path + ".gz");
+        gwriter.write("aaron's set\tthis is aaron's set\t51\nanother set\tyet another set\t82\n");
+    }
+    expect_error([&]() { gesel::internal::check_set_details(path, std::vector<int32_t>{ 31, 27, 42 }, std::vector<int32_t>{ 51, 82, 93 }); }, "number of lines");
+}
